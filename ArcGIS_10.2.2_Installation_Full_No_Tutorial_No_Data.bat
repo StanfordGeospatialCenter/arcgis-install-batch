864097a6cddce871bb4b16b2706754b4cec9a8be
Stace Maples <stacemaples@stanford.edu>
2015-03-16 13:59:37 -0700

    Changes to directory structure in paths
    Changed to reflect the assumption that users will download all 10.2.2
    media to a folder called ArcGIS_Desktop_10.2.2 and use WINRAR of 7Zip to
    "Extract All Here." Now using the directory structures created by that
    workflow.

--- ArcGIS_10.2.2_Installation_Full_No_Tutorial_No_Data.bat
+++ ArcGIS_10.2.2_Installation_Full_No_Tutorial_No_Data.bat
@@ -12,52 +12,54 @@ REM Determine if on 64-bit operating system
 IF not EXIST "C:\Program Files (x86)" GOTO 32BIT
 
 echo Installing the SQL Server Native Client on 64-bit Windows first...
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop102\DBMS_Support_Files\64-bit\sqlncli.msi" IACCEPTSQLNCLILICENSETERMS=YES /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\SQLServer2012SP1NativeClient_64\sqlncli.msi" IACCEPTSQLNCLILICENSETERMS=YES /norestart /passive /qb
 
 echo Installing MSXML6...
 rem install MSXML6
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop102\Desktop\Support\MSXML6\64-bit\msxml6_x64.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\Desktop\SetupFiles\Support\MSXML6\64-bit\msxml6_x64.msi" /norestart /passive /qb
 
 GOTO SQLEND
 
 :32BIT
 echo Installing the SQL Server Native Client on 32-bit Windows first...
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop102\DBMS_Support_Files\32-bit\sqlncli.msi" IACCEPTSQLNCLILICENSETERMS=YES /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\SQLServer2012SP1NativeClient_32\sqlncli.msi" IACCEPTSQLNCLILICENSETERMS=YES /norestart /passive /qb
 
 echo Installing MSXML6...
 rem install MSXML6
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop102\Desktop\Support\MSXML6\32-bit\msxml6.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\Desktop\SetupFiles\Support\MSXML6\32-bit\msxml6.msi" /norestart /passive /qb
 
 :SQLEND
 
 echo Installing ArcGIS Desktop 10.2.2... this could take a while... you should go have dinner, or something...
 rem Install ArcGIS Desktop 10.2.2  with options
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop_10.2.2\Desktop\SetupFiles\setup.msi" ADDLOCAL=ALL ESRI_LICENSE_HOST=arcgis.library.yale.edu INSTALLDIR1=C:\Python27 SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\Desktop\SetupFiles\setup.msi" ADDLOCAL=ALL ESRI_LICENSE_HOST=27004@sul-gislm.stanford.edu INSTALLDIR1=C:\Python27 SOFTWARE_CLASS=Professional SEAT_PREFERENCE=Float /norestart /passive /qb
 
 echo Installing VBA support for ArcGIS 10.2
 rem Install VBA Support for ArcGIS 10.2
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop_10.2.2\VBACompatibility\VBA\VBAOF11.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\VBACompatibility\VBA\VBAOF11.msi" /norestart /passive /qb
 
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop_10.2.2\VBACompatibility\VBA\1033\VBAOF11I.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\VBACompatibility\VBA\1033\VBAOF11I.msi" /norestart /passive /qb
 
 echo Installing the Data Interoperability Extension (you should read up on this, it's pretty cool)...
 rem Install Data Interoperability Extension for ArcGIS 10.2.2
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop_10.2.2\DataInterop\SetupFiles\setup.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\DataInteropDesktop\SetupFiles\setup.msi" /norestart /passive /qb
 
 REM Determine if on 64-bit operating system
 IF not EXIST "C:\Program Files (x86)" GOTO 64BITEND
 
 echo Installing the 64-bit Background Geoprocessing Extension...
 rem 64-bit Background Geoprocessing Extension
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_Desktop_10.2.2\BackgroundGP\SetupFiles\setup.msi" /norestart /passive /qb
+C:\WINDOWS\system32\msiexec.exe /I "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\DesktopBackgroundGP\SetupFiles\setup.msi" /norestart /passive /qb
 
 :64BITEND
 
-echo Installing ArcGIS Explorer 2500... Think of this as GeoPowerPoint...
-Rem Install ArcGIS Explorer 2500 
-C:\WINDOWS\system32\msiexec.exe /I "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGISExplorer\2500\Setup\AllUsers.msi" /qb /norestart
-
 Rem Install Registry Entries for enabling all extensions and setting Relative Paths as the default
-REGEDIT.exe -S "\\130.132.156.234\ArcGIS_10_Faculty_Staff\ArcGIS_10.2.2_RegistryEntries.reg" /norestart
+REGEDIT.exe -S "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\ArcGIS_10.2.2_RegistryEntries.reg" /norestart
 
+echo Installing ArcGIS 10.2 Tutorial Materials that go with the tutorials in the Help System...
+rem Install ArcGIS 10.2 Tutorial Materials
+xcopy "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\ArcTutor" "C:\ArcTutor" /s /e /i
 
+echo Installing the Esri Data & Maps Dataset... this is about 8GB of data, so it might take a while...
+rem Copy ESRI Data & Maps to the C: Drive of the target machine
+xcopy "\\INSERT_YOUR_SERVER_ADDRESS_HERE\ArcGIS_Desktop_10.2.2\DATAMAPS102" "C:\DATAMAPS102" /s /e /i 
